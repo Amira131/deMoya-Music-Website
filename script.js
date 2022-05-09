4b2abfb3a39791bbbd69f4b9a9d1dd4d08b217d9
Amira de Moya <demoyaamira@gmail.com>
2022-05-09 05:24:06 -0400

    commit message

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ const h2 = document.createElement("h2");
 h2.textContent = "The Girl In The Water";
 document.querySelector("body").appendChild(h2);
 
+const img = document.getElementById('img');
+function clickAlert() {
+    alert(' Just a girl in the water.')
+}
 
-
+img.addEventListener('click, clickAlert')
