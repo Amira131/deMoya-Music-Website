2dae3e928e71afbc88e9fa6ca8406b33f5436cbe
Amira de Moya <demoyaamira@gmail.com>
2022-05-10 00:29:55 -0400

    added h4 and h5 andupdated css for h3

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-const h2 = document.createElement("h2");
-h2.textContent = "The Girl In The Water";
-document.querySelector("body").appendChild(h2);
+const h4 = document.createElement("h4");
+h4.textContent = "Gallery";
+document.querySelector("body").appendChild(h4);
 
-let songPlayer = document.getElementById("song-player")
+let album = document.getElementById("album-cover")
 
-
-var audio = new Audio("How Can You Be Happy.wav");
-
-document.onclick = function() {
-  audio.play();
-}
-
-var sound = new Audio("How Can You Be Happy.wav");
-
-songPlayer.addEventListener('click',()=>{
-	sound.currentTime = 0;
-	sound.play();
+album.addEventListener('click',()=>{
+    console.log("clicked")
 });
 
-//test
+
